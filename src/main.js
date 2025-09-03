229f088ffe33628fd682a9787cb230b81e1f2816
BlineraMoberg <bm222mr@student.lnu.se>
2025-09-03 14:30:18 +0200

    Add joke display and improve greeting message in index.html and main.js

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
   const nameInput = document.getElementById('name-Input')
   const knapp = document.getElementById('halsning-btn')
   const meddelandeDiv = document.getElementById('meddelande')
+  const jokeDiv = document.getElementById('joke')
 
-  function skapaHalsning(name) {
-    return `Hej ${name}, välkommen!`
-  }
+  // function skapaHalsning(name) {
+  //   return `Hej ${name}, välkommen!`
+  // }
 
   knapp.addEventListener('click', function () {
     const name = nameInput.value.trim()
 
     if (name) {
-      meddelandeDiv.innerHTML = `Hej ${name}! välkommen tillbaka till kodandet!`
+      meddelandeDiv.textContent = `Hej ${name}! välkommen tillbaka till kodandet!`
+
+      jokeDiv.innerHTML = `Knack knack, ${name}, vem där?`
+
       meddelandeDiv.style.display = 'block'
     } else {
       alert('Skriv ditt namn först!')
